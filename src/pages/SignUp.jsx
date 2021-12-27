bd82ad1b371ec79c3cb208bb50edd8910c9bcbd0
Kumar Sumit <kumarsumit1989@gmail.com>
2021-12-27 11:51:39 +0530

    Reguster new user to firebase authentication

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { db } from "../firebase.config";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilyIcon from "../assets/svg/visibilityIcon.svg";
 
@@ -19,57 +25,76 @@ const SignUp = () => {
       [e.target.id]: e.target.value,
     }));
   };
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+      updateProfile(auth.currentUser, {
+        displayName: name,
+      });
+      navigate("/");
+    } catch (error) {
+      console.log(error.code);
+      console.log(error.message);
+    }
+  };
   return (
     <>
       <div className="pageContainer">
         <header>
           <div className="pageHeader">Welcome Back ...</div>
         </header>
-        <main>
-          <form>
+
+        <form onSubmit={onSubmit}>
+          <input
+            className="nameInput"
+            type="text"
+            placeholder="Name"
+            id="name"
+            value={name}
+            onChange={onChange}
+          />
+          <input
+            className="emailInput"
+            type="email"
+            placeholder="Email"
+            id="email"
+            value={email}
+            onChange={onChange}
+          />
+          <div className="passwordInputDiv">
             <input
-              className="nameInput"
-              type="text"
-              placeholder="Name"
-              id="name"
-              value={name}
+              type={showPassword ? "text" : "password"}
+              className="passwordInput"
+              placeholder="Password"
+              id="password"
+              value={password}
               onChange={onChange}
             />
-            <input
-              className="emailInput"
-              type="email"
-              placeholder="Email"
-              id="email"
-              value={email}
-              onChange={onChange}
+            <img
+              src={visibilyIcon}
+              alt="show Password"
+              className="showPassword"
+              onClick={() => setShowPassword((prevState) => !prevState)}
             />
-            <div className="passwordInputDiv">
-              <input
-                type={showPassword ? "text" : "password"}
-                className="passwordInput"
-                placeholder="Password"
-                id="password"
-                value={password}
-                onChange={onChange}
-              />
-              <img
-                src={visibilyIcon}
-                alt="show Password"
-                className="showPassword"
-                onClick={() => setShowPassword((prevState) => !prevState)}
-              />
-            </div>
-            <Link to="/forgot-password" className="forgotPasswordLink">
-              Forgot Password
-            </Link>
-            <div className="signUpBar">
-              <p className="signUpText">Sign Up</p>
-              <button className="signUpButton">
-                <ArrowRightIcon fill="#ffffff" width="34px" height="34px" />
-              </button>
-            </div>
-          </form>
-        </main>
+          </div>
+          <Link to="/forgot-password" className="forgotPasswordLink">
+            Forgot Password
+          </Link>
+          <div className="signUpBar">
+            <p className="signUpText">Sign Up</p>
+            <button className="signUpButton">
+              <ArrowRightIcon fill="#ffffff" width="34px" height="34px" />
+            </button>
+          </div>
+        </form>
+
         {/* Google Oath */}
         <Link to="/sign-in" className="registerLink">
           Sign In Instead
